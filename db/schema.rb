3e6f894b86b505e8933abf7505568997fd0c1a22
Ali <ali.galway2002@gmail.com>
2022-07-02 10:22:56 +0100

    created post table, added link for post to signed in user page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_02_070728) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_02_082629) do
+  create_table "posts", force: :cascade do |t|
+    t.string "user"
+    t.date "post_date"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
